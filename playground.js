39cabf6ef21b580711c06d92d841cd54d4c8f4ab
spahicadev <aspahic@student.unipu.hr>
2024-09-30 15:08:21 +0200

    I will commit these because of good achivment for me, thinking! lines 39-47 I organize code to render imgs(MVC) with DRY, so to achive that i make a function with parameter and use that parameter for multiple situations

--- playground.js
+++ playground.js
@@ -11,6 +11,7 @@ const AvgWinsComputer = document.querySelector('.computer-avg-wins');
 const movesYou = document.querySelectorAll('.moves');
 const chosingYou = document.querySelector('.chosed-move-you');
 const movesComputer = document.querySelectorAll('.chosed-move-computer');
+const computerBoxMoves = document.querySelector('.computer-box');
 
 //btns
 backToHomepage.addEventListener('click', () => {
@@ -35,6 +36,15 @@ function getRandomMove() {
   return computerMove = moves[Math.floor(Math.random() * moves.length)];
 }
 
+function renderMoveIcon(move) {
+  if(computerMove === move) {
+    movesComputer[0].innerHTML = move
+    movesComputer[1].remove()
+    const img = document.createElement('img');
+    img.setAttribute('src', `./assets/${move}.svg`);   
+    computerBoxMoves.innerHTML = `<img class="moves" src=${img.src}>`
+}
+}
 
 function playingGame() {
 movesYou.forEach((move) => {
@@ -44,6 +54,7 @@ movesYou.forEach((move) => {
       chosingYou.innerHTML = playerMove;
       console.log(playerMove);
       getRandomMove()
+      renderMoveIcon(computerMove);
       console.log(computerMove);
       if(playerMove === computerMove) {
         console.log('TIE');
@@ -54,16 +65,34 @@ movesYou.forEach((move) => {
       }
     }
     else if(e.target.id === 'paper') {
-      playerMove = e.target.id
+      playerMove = e.target.id;
       console.log(playerMove);
+      chosingYou.innerHTML = playerMove;
       getRandomMove()
+      renderMoveIcon(computerMove);
       console.log(computerMove);
+      if(playerMove === computerMove) {
+        console.log('TIE');
+      } else if(playerMove === 'paper' && computerMove === 'rock') {
+        console.log('Computer win');
+      } else if(playerMove === 'paper' && computerMove === 'scissors') {
+        console.log('Computer win')
+      }
     }
     else if(e.target.id === 'scissors') {
       playerMove = e.target.id
+      chosingYou.innerHTML = playerMove;
       console.log(playerMove);
       getRandomMove()
+      renderMoveIcon(computerMove);
       console.log(computerMove);
+      if(playerMove === computerMove) {
+        console.log('TIE');
+      } else if(playerMove === 'scissors' && computerMove === 'rock') {
+        console.log('Computer win');
+      } else if(playerMove === 'scissors' && computerMove === 'paper') {
+        console.log('Player win')
+      }
     }
   })
 })
